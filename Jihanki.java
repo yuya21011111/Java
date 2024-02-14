a775a21dc0238004da694263c32dae14dab0a227
YuyaYamazi <kaimomo12345@gmail.com>
2024-02-14 15:34:45 +0900

    学習day8

---
+++ Jihanki.java
@@ -0,0 +1,30 @@
+package Java学習;
+
+import java.util.Scanner;
+
+public class Jihanki {
+    Scanner scanner = new Scanner(System.in);
+		 System.out.print("配列のサイズを数字で指定してください：");
+	        int size = scanner.nextInt();
+	        int[] array = new int[size];
+	        int price = 0;
+
+	        System.out.println("硬貨を入れてください:");
+	        for (int i = 0; i < size; i++) {
+	            array[i] = scanner.nextInt();
+	        }
+	        
+	        for(int j = 0; j < array.length; j++) {
+	        	if(array[j] == 1 || array[j] == 5) {
+	        		System.out.println("警告：" + array[j] + "円玉は使えません。");
+	        	}
+	        	else if(array[j] == 10 || array[j] == 50 || array[j] == 100 || array[j] == 500) {
+	        		price += array[j];
+	        	}
+	        	else {
+	        		System.out.println(array[j] + "は硬貨として適切な値ではありません。");
+	        	}
+	        }
+	        
+	        System.out.println("ただいまの投入金額は" + price + "円です。");
+}
