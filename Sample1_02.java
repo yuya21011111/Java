e189e53cc8c1e55be8332344f5075e25a5752a56
YuyaYamazi <kaimomo12345@gmail.com>
2024-02-16 16:23:10 +0900

    学習day10

--- Sample1_02.java
+++ Sample1_02.java
@@ -3,7 +3,7 @@ package Java学習;
 import java.util.Scanner;
 
 public class Sample1_02 {
-    // メソッド
+    // メソッドです
     public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		 Scanner scanner = new Scanner(System.in);
