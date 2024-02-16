c809924c973d880b09cb06b0114e7f209ec40ff5
YuyaYamazi <kaimomo12345@gmail.com>
2024-02-16 16:21:10 +0900

    学習day10

---
+++ Sample1_02.java
@@ -0,0 +1,26 @@
+package Java学習;
+
+import java.util.Scanner;
+
+public class Sample1_02 {
+    // メソッド
+    public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		 Scanner scanner = new Scanner(System.in);
+	     int[] numbers = new int[2];
+	        System.out.println("足し算する数を２つ入力してね:");
+	        for (int i = 0; i < 2; i++) {
+	        	numbers[i] = scanner.nextInt();
+	        }
+		int num = sum(numbers[0],numbers[1]);
+		System.out.println("合計は" + num + "です。");
+	
+		}
+	
+	public static int sum(int i1, int i2) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		int result = i1 + i2;
+		return result;
+	}
+}
