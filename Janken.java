4ec7289ceb46a3a688572ef4c78bbc1480acdda0
YuyaYamazi <kaimomo12345@gmail.com>
2024-02-20 16:06:33 +0900

    Java学習day11

--- Janken.java
+++ Janken.java
@@ -8,13 +8,17 @@ public class Sample03 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		int hand_number = 0;
+		String prayer_hand;
+		String cpu_hand;
 		while(hand_number != 4) {
 			Scanner scanner = new Scanner(System.in);
 			System.out.println("じゃんけんを始めるよ！番号を入力してね： 1:グー 2:チョキ 3: パー 4: ゲームを終わる");
 		    hand_number = scanner.nextInt();
 			Random random = new Random();
 			int randomNumberCPU = random.nextInt(3) + 1;
-			 
+			prayer_hand = getHand(hand_number);
+			
+			
 			 if(hand_number == randomNumberCPU) {
 				 System.out.println("あいこです");
 			 }
@@ -38,6 +42,17 @@ public class Sample03 {
 			 }
 			 
 		}
+		System.out.println("ゲームを終了します。");
+	}
+
+	public static String getHand(int hand_number) {
+		// TODO 自動生成されたメソッド・スタブ
+		String hand ;
+		switch(hand_number) {
+		case 1:
+			 hand = "グー";
+		}
+		return null;
 	}
 
 }
