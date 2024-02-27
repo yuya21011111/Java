36184d53c6a3ae0e083c28e8436757c1c954c1b7
YuyaYamazi <kaimomo12345@gmail.com>
2024-02-27 16:25:55 +0900

    Java学習day13

--- Sample07.java
+++ Sample07.java
@@ -5,6 +5,7 @@ public class Sample07 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		Sample07_car mocoCar = new Sample07_car();
+		mocoCar.carName();
 	}
 
 }
---
+++ Sample07_car.java
@@ -0,0 +1,19 @@
+package Java学習;
+
+public package sample;
+
+class Sample07_car {
+	String carModel = "TOYOTA";
+	String owner =  "ホンダ";
+	String color = "Green";
+	int speed = 0;
+	boolean right = false;
+	
+	// メソッド
+	void carName() {
+		System.out.println("車種名は" + carModel + "です");
+	}
+}
+ {
+    
+}
