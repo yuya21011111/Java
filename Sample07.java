e50c891ce609185614f4dfc65b6eab1d5a0bc0dd
YuyaYamazi <kaimomo12345@gmail.com>
2024-02-28 15:56:16 +0900

    コンストラクタで作るじゃんけんゲーム

--- Sample07.java
+++ Sample07.java
@@ -4,8 +4,15 @@ public class Sample07 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		Sample07_car mocoCar = new Sample07_car();
+		Sample07_car mocoCar = new Sample07_car("ホンダ","ムチ","RED",100,true);
+		Sample07_car kazuCar = new Sample07_car();
 		mocoCar.carName();
+		kazuCar.carName();
+		
+//		mocoCar.accelerator();
+		mocoCar.brake();
+		System.out.println(mocoCar.speed);
+		System.out.println(kazuCar.speed);
 	}
 
 }
--- Sample07_car.java
+++ Sample07_car.java
@@ -1,19 +1,41 @@
-package Java学習;
-
-public package sample;
+package sample;
 
 class Sample07_car {
-	String carModel = "TOYOTA";
-	String owner =  "ホンダ";
-	String color = "Green";
-	int speed = 0;
-	boolean right = false;
+	String carModel;
+	String owner;
+	String color;
+	int speed;
+	boolean right;
 	
+	Sample07_car(){
+		 carModel = "未登録";
+		 owner =  "未登録";
+		 color = "未登録";
+		 speed = 0;
+		 right = false;
+	}
+	
+	public Sample07_car(String string, String string2, String string3, int i, boolean b) {
+		// TODO 自動生成されたコンストラクター・スタブ
+		carModel = string;
+		 owner =  string2;
+		 color = string3;
+		 speed = i;
+		 right = b;
+	}
+
 	// メソッド
 	void carName() {
 		System.out.println("車種名は" + carModel + "です");
 	}
-}
- {
-    
+	
+	void accelerator() {
+		speed++;
+		System.out.println("スピードが上がった");
+	}
+	
+	void brake() {
+		speed--;
+		System.out.println("スピードが下がった");
+	}
 }
