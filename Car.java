bdd497c7e2799f51a71c8fea9c11e30a56c4dadc
YuyaYamazi <kaimomo12345@gmail.com>
2024-03-03 16:55:21 +0900

    カプセル化について

---
+++ Car.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Car {
+
+	
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Carcolor car = new Carcolor("トヨタ","RED");
+		
+		car.getColor("GOLD");
+	}
+
+}
---
+++ Carcolor.java
@@ -0,0 +1,36 @@
+package sample;
+
+class Carcolor {
+	private String color;
+	private String name;
+	private String car_color;
+	
+	public Carcolor() {
+		color = "未登録";
+		name = "未登録";
+	}
+	
+	public Carcolor(String string, String string2) {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public void getColor(String string) {
+		// TODO 自動生成されたメソッド・スタブ
+		if(searchColor(string)) {
+			System.out.print(string);
+		}
+		
+	}
+
+	private boolean searchColor(String string) {
+		// TODO 自動生成されたメソッド・スタブ
+		boolean col = false;
+		if(string.equals("GOLD")) {
+			col = true;
+		}
+		return col;
+	}
+	
+	
+	
+}
